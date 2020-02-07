33bf3523e4173f4ea8fad505ef56334b9406ed5a
Siddharth Venu <siddharthvvenu@gmail.com>
2020-02-08 04:08:56 +0530

    Add marker in the centre

--- app/src/main/java/io/github/intimidate/decamin/BookRideActivity.java
+++ app/src/main/java/io/github/intimidate/decamin/BookRideActivity.java
@@ -20,7 +20,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Objects;
 
