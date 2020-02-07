4d0772e2e124e89188e5494c9d5605d0e148962d
Siddharth Venu <siddharthvvenu@gmail.com>
2020-02-08 01:25:36 +0530

    Init repo

---
+++ app/src/main/java/io/github/intimidate/decamin/BookRideActivity.java
@@ -0,0 +1,14 @@
+package io.github.intimidate.decamin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookRideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_ride);
+    }
+}
