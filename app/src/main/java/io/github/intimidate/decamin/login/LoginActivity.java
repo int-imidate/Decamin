bc9441e3fa20bde798c7587a3c9886165781bd71
Gansta <harithlaxman@gmail.com>
2020-02-08 17:43:00 +0530

    Fixed the bugs sid

--- app/src/main/java/io/github/intimidate/decamin/login/LoginActivity.java
+++ app/src/main/java/io/github/intimidate/decamin/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
